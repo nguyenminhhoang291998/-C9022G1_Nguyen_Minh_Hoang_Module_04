29f577e3801e85e52206d55b11b4f5f2874e750a
nguyenminhhoang291998 <nguyenminhhoang291998@gmail.com>
2023-02-15 11:09:14 +0700

    ss11

--- ss5/exercise/blog/blog/src/main/java/com/example/blog/repository/ICategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blog.repository;
-
-import com.example.blog.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category,Integer> {
-
-}
